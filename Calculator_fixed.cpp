57cfea197a91dc0d85eaacb39bda4dbb43ff039a
ClaudioGM <168583627+CladioGM-16@users.noreply.github.com>
2025-01-25 19:27:09 -0500

    Update Calculator_fixed.cpp

--- Calculator_fixed.cpp
+++ Calculator_fixed.cpp
@@ -3,7 +3,6 @@
 #include <chrono>
 #include <unistd.h>
 #include <mutex>
-
 std::mutex io_mutex;
 
 void calculator(const char* name) {
@@ -12,7 +11,6 @@ void calculator(const char* name) {
     while (true) {
         int num1, num2;
         {
-            // Bloque sincronizado para proteger entrada y salida
             std::lock_guard<std::mutex> lock(io_mutex);
             std::cout <<"Enter first number: ";
             std::cin >> num1;
@@ -20,8 +18,6 @@ void calculator(const char* name) {
             std::cin >> num2;
             std::cout<<"Result: " << num1 + num2 << std::endl<< std::endl;
         }
-
-        // Pausa para evitar que los hilos se ejecuten muy rápido
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
@@ -29,11 +25,8 @@ void calculator(const char* name) {
 int main() {
     printf("Main Process ID: %d\n", getpid());
     printf("Main Thread ID: %ld\n", std::this_thread::get_id());
-
-    // Crear los hilos
     std::thread thread1(calculator, "C1");
     std::thread thread2(calculator, "C2");
-
     while (true) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
